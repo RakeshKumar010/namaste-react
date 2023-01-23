f100eaafc04eed46b9280a5ab076e56ea486d4d8
Rakseh <rakeshkumar748844@gmail.com>
2023-01-23 10:18:58 +0530

    class-1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading1 = React.createElement('h1', {}, 'namaste heading 1')
+const heading2 = React.createElement('h2', {}, 'namaste heading 2')
+const Heading3 = React.createElement('h3', {}, 'namaste headiing 3')
+const MainDiv = React.createElement('div', { id: 'maindiv' }, [heading1, heading2, Heading3])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(MainDiv);
