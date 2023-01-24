7ab9de6c24a63f00c33516287d8026f3f4acacb6
Rakseh <rakeshkumar748844@gmail.com>
2023-01-24 18:45:46 +0530

    01/01/23 Half class

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-const heading1 = React.createElement('h1', {}, 'namaste heading 1')
-const heading2 = React.createElement('h2', {}, 'namaste heading 2')
-const Heading3 = React.createElement('h3', {}, 'namaste headiing 3')
-const MainDiv = React.createElement('div', { id: 'maindiv' }, [heading1, heading2, Heading3])
-console.log(MainDiv)
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(MainDiv);
+import ReactDOM from 'react-dom'
+import './index.css'
+// const heading =React.createElement('h1',{id:"maindiv"},'hello world')
+                         //Both are same
+const heading=<h1 id="maindiv">Hello world</h1>
+console.log(heading)
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
